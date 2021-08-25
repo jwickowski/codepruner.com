c4fa019241058cf0eb2e987ea3e06709d32c749c
Jerzy Wickowski <jurek@deployacademy.pl>
2021-08-25 12:52:00 +0200

    create TypeScriptModelGenerator

---
+++ src/examples/CodePruner.Examples/CodePruner.Examples.TypeScriptCodeGenerators/TypeScriptModelGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePruner.Examples.TypeScriptCodeGenerators
+{
+    public class TypeScriptModelGenerator
+    {
+        private BackendFieldGetter backendFieldGetter;
+        private TypeScriptContentGenerator typeScriptContentGenerator;
+
+        public TypeScriptModelGenerator()
+        {
+            backendFieldGetter = new BackendFieldGetter();
+            typeScriptContentGenerator = new TypeScriptContentGenerator();
+        }
+
+        public string GenerateTypeScriptModel(Type type)
+        {
+            string typeName = type.Name;
+            var backendFields = backendFieldGetter.GetBackendField(type);
+            var content = typeScriptContentGenerator.GenerateModel(typeName, backendFields);
+            return content;
+        }
+    }
+}
