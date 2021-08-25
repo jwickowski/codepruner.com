e8ebffb050563db0333f78b390743dca88b53cd0
Jerzy Wickowski <jurek@deployacademy.pl>
2021-08-25 12:46:49 +0200

    add a draft of TypeScriptModelGenerator

---
+++ src/examples/CodePruner.Examples/CodePruner.Examples.TypeScriptCodeGenerators/TypeScriptModelGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodePruner.Examples.TypeScriptCodeGenerators
+{
+    public class TypeScriptModelGenerator
+    {
+        public string GenerateModel(string className, IEnumerable<BackendField> backendFields)
+        {
+            var sb = new StringBuilder();
+            sb.Append($"export type {className} {{");
+            foreach (var backendField in backendFields)
+            {
+                var frontendType = GetFrontendType(backendField);
+                sb.Append($"  {backendField.Name}: {frontendType}");
+            }
+
+            sb.Append($"  ");
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+
+        private string GetFrontendType(BackendField backendField)
+        {
+            switch (backendField.Type)
+            {
+                case "Int32":
+                    return "number";
+                case "String":
+                    return "string";
+            }
+
+            return backendField.Type;
+        }
+    }
+}
