eafe55c5f8f6955a27fed11e835a9d4c05e99728
Jerzy Wickowski <jurek@deployacademy.pl>
2021-08-27 09:09:18 +0200

    add a runner for typescript generator

---
+++ src/examples/CodePruner.Examples/CodePruner.Examples.TypeScriptCodeGenerators.Runner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace CodePruner.Examples.TypeScriptCodeGenerators.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start: CodePruner.Examples.TypeScriptCodeGenerators");
+            var typeScriptModelGenerator = new TypeScriptModelGenerator();
+            var content = typeScriptModelGenerator.GenerateTypeScriptModel(typeof(AccountDto));
+            File.WriteAllText("AccountDto.generated.ts", content);
+        }
+    }
+
+    public class AccountDto
+    {
+        public int AccountId { get; set; }
+        public string DisplayName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
