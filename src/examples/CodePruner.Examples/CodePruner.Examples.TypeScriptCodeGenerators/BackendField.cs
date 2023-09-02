b9d8d1a4e44116c651c8e22e64a8a85da3587629
Jerzy Wickowski <jerzy.wickowski@esatto.se>
2023-09-02 08:10:58 +0200

    remove old not used files in src/examples/**

--- src/examples/CodePruner.Examples/CodePruner.Examples.TypeScriptCodeGenerators/BackendField.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CodePruner.Examples.TypeScriptCodeGenerators
-{
-    internal struct  BackendField
-    {
-        public string Name { get; set; }
-        public string Type { get; set; }
-    }
-}
