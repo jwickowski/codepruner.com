511471326050e3950e0579af424e8e7ba906d5cf
Jerzy Wickowski <jerzy.wickowski@esatto.se>
2023-10-10 06:08:01 +0200

    add a base for .net open ai

---
+++ src/codepruner.com/static/examples/CodePruner.Examples/CodePruner.Examples.AI.ChatGPT/OpenAIClientFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace CodePruner.Examples.AI.ChatGPT;
+
+public static class OpenAIClientFactory
+{
+    private const string apiKey = "OPEN_AI_API_KEY";
+    public static OpenAIClient Create()
+    {
+        return new OpenAIClient(apiKey);
+    }
+}
