3cd72840d64b0de12800fccdee8e587f479c31b4
Jerzy Wickowski <jerzy.wickowski@esatto.se>
2023-10-30 07:35:27 +0100

    add project for examples

---
+++ src/codepruner.com/static/examples/CodePruner.Examples/CodePruner.Examples.AI.ExploreSemanticKernel/ExploringSemanticKernel.cs
@@ -0,0 +1,13 @@
+namespace CodePruner.Examples.AI.ExploreSemanticKernel;
+
+public class ExploringSemanticKernel
+{
+    #region create_semantic_kernel
+    [Fact]
+    public void HowToCreateSemanticKernel()
+    {
+        
+    }
+    
+    #endregion
+}
