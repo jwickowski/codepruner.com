133d982351394c3dfaffdadcf6db8ce425c94f6f
Jerzy Wickowski <jerzy.wickowski@esatto.se>
2023-10-31 12:52:56 +0100

    a

---
+++ src/codepruner.com/content/english/post/Untitled1.ps1
@@ -0,0 +1,36 @@
+﻿$path = "C:\Projects\jw\codepruner.com\src\codepruner.com\content\posts"
+$items = Get-ChildItem -Path $path -Recurse -Filter *.md
+foreach($item in $items)
+{
+    $fileName = $item.FullName
+    
+    $url = $fileName.Replace("C:\Projects\jw\codepruner.com\src\codepruner.com\content\","")
+    $url = $url.Replace(".md","")
+    $url = $url.Replace("\","/")
+
+    $category = $url.Replace("posts/","");
+    $category = $category.Substring(0,$category.IndexOf("/"))
+    $category = $category.ToLower()
+    
+    #Write-Host $category
+
+    (Get-Content $fileName) | 
+    Foreach-Object {
+        $_ # send the current line to output
+        if ($_ -match "categories:") 
+        {
+            #Add Lines after the selected pattern 
+            #"url: ""$url"""
+            #"images:"
+            #"  - ""images/blog/red-sharp-pruner.jpg"""
+            #"author: ""Jerzy Wickowski"""
+           # "categories: [""$category""]"
+            "type: ""regular"""
+        }
+    } | Set-Content $fileName
+
+
+}
+
+
+Write-Host $items.Length
---
+++ src/tools/Set-DisqusFrontMatterForEachPost.ps1
@@ -0,0 +1,83 @@
+﻿
+ 
+#  param (
+#         [Parameter(Mandatory)]
+#         [string] 
+#         $path
+#        )
+
+     
+
+function Add-DisqusDataToFrontMatter {
+    param (
+        [Parameter(Mandatory)]
+         [string] 
+         $Path
+    )
+
+    }
+
+
+    function Add-DisqusDataToFrontMatterForOneFile {
+    param (
+        [Parameter(Mandatory)]
+         [string] 
+         $FilePath
+    )
+   $urlExists = $false;
+   $url = "";
+    
+    (Get-Content $FilePath) | 
+    Foreach-Object {
+        if ($_ -match "url:") 
+        {
+              $url = $_.Replace("url:", "").Replace('"','').Trim();
+            $urlExists = $true;
+            #Add Lines after the selected pattern 
+            #"url: ""$url"""
+            #"images:"
+            #"  - ""images/blog/red-sharp-pruner.jpg"""
+            #"author: ""Jerzy Wickowski"""
+           # "categories: [""$category""]"
+            
+        }
+    }
+
+    if($url -eq ""){
+
+    $url = $FilePath.Replace("C:\Projects\jw\codepruner.com\src\codepruner.com\content\","")
+    $url = $url.Replace(".md","")
+    $url = $url.Replace("\","/")
+        }
+
+
+    #Write-Host $category
+
+   
+   
+   return
+   
+    (Get-Content $fileName) | 
+    Foreach-Object {
+        $_ # send the current line to output
+        if ($_ -match "categories:") 
+        {
+            #Add Lines after the selected pattern 
+            #"url: ""$url"""
+            #"images:"
+            #"  - ""images/blog/red-sharp-pruner.jpg"""
+            #"author: ""Jerzy Wickowski"""
+           # "categories: [""$category""]"
+            "type: ""regular"""
+        }
+    } | Set-Content $fileName
+
+
+    }
+
+
+
+
+     Add-DisqusDataToFrontMatterForOneFile -FilePath "C:\Projects\jw\codepruner.com\src\codepruner.com\content\english\post\2023\2023-09-22-how-to-run-postgresql-and-adminer-or-pgadmin-with-docker-compose.mdt"
+
+
