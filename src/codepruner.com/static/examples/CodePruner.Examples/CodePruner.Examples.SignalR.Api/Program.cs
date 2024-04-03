c78df7fd1001770b1f01d8789e97db3837c97965
Jerzy Wickowski <jerzy.wickowski@esatto.se>
2024-04-03 11:35:59 +0200

    enable cors

--- src/codepruner.com/static/examples/CodePruner.Examples/CodePruner.Examples.SignalR.Api/Program.cs
+++ src/codepruner.com/static/examples/CodePruner.Examples/CodePruner.Examples.SignalR.Api/Program.cs
@@ -7,6 +7,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod().
+                AllowCredentials();
+        });
+});
 var app = builder.Build();
 
 
@@ -17,8 +28,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors();
 
 app.UseHttpsRedirection();
+
+
 app.MapHub<ProcessingHub>("/ProcessingHub");
 app.MapHub<StronglyTypedProcessingHub>("/StronglyTypedProcessingHub");
 var summaries = new[]
